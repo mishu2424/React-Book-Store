cc548bd94edd903469b3b2856f06e41c1dcde7fe
Apurbo dey <apurbomishu24@gmail.com>
2025-02-24 11:42:19 -0500

    sort option has been added

--- src/Components/ListedBooks/ListedBooks.jsx
+++ src/Components/ListedBooks/ListedBooks.jsx
@@ -8,14 +8,15 @@ const ListedBooks = () => {
   const [lists, setLists] = useState([]);
   const [readZone, setReadZone] = useState("readLists");
   const [loading, isLoading] = useState(false);
-  //   console.log(readZone);
+  const [sortText, setSortText] = useState('');
+
   const books = useLoaderData();
-  console.log(books);
   const showLists = (list) => {
     isLoading(true);
     setLists([]);
     const getLists = getStoredList(list);
-    // console.log(getLists);
+    // If we want the sort to be reset, we can use this
+    // setSortText('');
     const newLists = [];
     getLists.forEach((list) => {
       const selectedBooks = books.find((book) => book.bookId === Number(list));
@@ -23,6 +24,9 @@ const ListedBooks = () => {
     });
     setTimeout(() => {
       setLists(newLists);
+      if(sortText){
+        handleSortText(sortText, newLists);
+      }
       isLoading(false);
     }, 1000);
   };
@@ -39,8 +43,43 @@ const ListedBooks = () => {
       isLoading(false);
     }, 2000);
   }, []);
+
+  const handleSortText=(sortType, lists)=>{
+    setSortText(sortType);
+    if(sortType==='Ratings'){
+        lists.sort((a,b)=>{
+            return b.rating - a.rating;
+        })
+        setLists(lists);
+    }else{
+        lists.sort((a,b)=>{
+            return b.totalPages - a.totalPages;
+        })
+        setLists(lists);
+    }
+  }
+
+
   return (
     <div className="w-[80vw] lg:w-[70vw] mx-auto my-5">
+      <div className="w-fit mx-auto">
+      <div className="dropdown dropdown-start">
+        <div tabIndex={0} role="button" className="btn m-1 bg-[#23BE0A] text-white">
+          {sortText ? `Sort by : ${sortText}` : 'Sort By'} ⬇️
+        </div>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+        >
+          <li onClick={()=>handleSortText('Ratings', lists)}>
+            <a>Ratings</a>
+          </li>
+          <li onClick={()=>handleSortText('No of Pages', lists)}>
+            <a>No of pages</a>
+          </li>
+        </ul>
+      </div>
+      </div>
       <div className="flex items-center join border-b border-gray-200">
         <div
           onClick={() => {
